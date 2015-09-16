9b30376ab249f71e2df99da10f24cc0305d6d74a
José Nicolás Silva Schwarzenberg <zar.nicolas20@gmail.com>
2015-09-16 13:39:28 -0700

    Initial Commit
    Explicit Path

---
+++ GlyphsScripts_updater.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+cd GlyphsScripts/BelaFrank_StringSmash/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../DeutschMark_Glyphsapp-Plugins/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../DeutschMark_Glyphsapp-Scripts/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../jenskutilek_Curve-Equalizer/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../jenskutilek_RedArrow-Glyphs/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../justanotherfoundry_glyphsapp-scripts/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_BroadNibber/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_CutAndShake/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_FixZeroHandles/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_Glyphs-Scripts/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_InsertInflections/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_Inverter/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_LayerGeek/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_MakeCorner/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_Noodler/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowAngledHandles/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowComponentOrder/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowCoordinatesOfSelectedNodes/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowCrosshair/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowFilledPreview/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowGlyphsInLabelColor/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowHandlesEverywhere/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowInterpolations/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowMetricsKeys/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../mekkablue_ShowOffsetCurveParameterPreview/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../schriftgestalt_Autopsy-Plugin/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../schriftgestalt_Glyphs-Plugins/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../schriftgestalt_Glyphs-Scripts/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../schriftgestalt_word-o-mat/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../Tosche_BubbleKern/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../Tosche_Glyphs-Scripts/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+cd ../Tosche_ShowBlackFill/
+git pull
+printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+echo "Sync all repositories"
