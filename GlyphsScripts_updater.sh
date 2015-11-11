95899b57b93c4b210900abd7fcb4fdaf607fb330
José Nicolás Silva Schwarzenberg <zar.nicolas20@gmail.com>
2015-11-11 09:20:13 -0800

    Change syntax and updated to new repositories

--- GlyphsScripts_updater.sh
+++ GlyphsScripts_updater.sh
@@ -1,126 +1,164 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-cd GlyphsScripts/BelaFrank_StringSmash/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../DeutschMark_Glyphsapp-Plugins/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../DeutschMark_Glyphsapp-Scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../HuertaTipografica_Scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../jenskutilek_Curve-Equalizer/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../jenskutilek_RedArrow-Glyphs/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../justanotherfoundry_glyphsapp-scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_BroadNibber/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_CutAndShake/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_FixZeroHandles/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_Glyphs-Scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_InsertInflections/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_Inverter/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_LayerGeek/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_MakeCorner/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_NotePalettes/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_Noodler/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowAngledHandles/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowComponentOrder/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowCoordinatesOfSelectedNodes/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowCrosshair/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowExportStatus/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowFilledPreview/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowGlyphsInLabelColor/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowHandlesEverywhere/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowInterpolations/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowMetricsKeys/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_ShowOffsetCurveParameterPreview/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../mekkablue_Symmetry/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../schriftgestalt_Autopsy-Plugin/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../schriftgestalt_DrawBotGlyphsPlugin/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../schriftgestalt_Glyphs-Plugins/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../schriftgestalt_Glyphs-Scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../schriftgestalt_word-o-mat/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../SimonCozens_GlyphsGit/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../SimonCozens_Scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../SimonCozens_ShowPathArea/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../Tosche_BubbleKern/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../Tosche_Glyphs-Scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../Tosche_ShowBlackFill/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-cd ../wei_scripts/
-git pull
-printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
-echo "Sync all repositories"
+if [ -d ~/Documents/GlyphsScripts ] ; then
+	cd ~/Documents/GlyphsScripts/BelaFrank_StringSmash/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Glyphsapp-Scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Distance-And-Angle-Of-Nodes/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Kerning-Group-Reference/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Label-Color/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Next-Master/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Node-Count/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Rotated/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Siblings/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Show-Smart-Plumblines/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/DeutschMark_Uncover-xHeight/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/HuertaTipografica_Scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/jenskutilek_Curve-Equalizer/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/jenskutilek_RedArrow-Glyphs/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/justanotherfoundry_glyphsapp-scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_BroadNibber/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_CutAndShake/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_FixZeroHandles/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_Glyphs-Scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_InsertInflections/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_Inverter/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_LayerGeek/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_MakeCorner/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_Noodler/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_NotePalettes/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowAngledHandles/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowComponentOrder/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowCoordinatesOfSelectedNodes/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowCrosshair/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowExportStatus/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowFilledPreview/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowGlyphsInLabelColor/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowHandlesEverywhere/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowInterpolations/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowMetricsKeys/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_ShowOffsetCurveParameterPreview/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/mekkablue_Symmetry/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/schriftgestalt_Autopsy-Plugin/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/schriftgestalt_DrawBotGlyphsPlugin/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/schriftgestalt_Glyphs-Plugins/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/schriftgestalt_Glyphs-Scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/schriftgestalt_word-o-mat/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/SimonCozens_GlyphsGit/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/SimonCozens_Scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/SimonCozens_ShowPathArea/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/Tosche_BubbleKern/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/Tosche_Glyphs-Scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/Tosche_ShowBlackFill/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	cd ~/Documents/GlyphsScripts/wei_scripts/
+	git pull
+	printf '\n\n\t#### Pulled %s\n\n' "${PWD##*/}"
+	echo ""
+	echo "###############################"
+	echo "# All repositories up-to-date #"
+	echo "###############################"
+fi
+if [ ! -d ~/Documents/GlyphsScripts ] ; then
+	echo ""
+	echo "##########################################"
+	echo "# GlyphsScripts is not in expected place #"
+	echo "# Check install instructions at:         #"
+	echo "#                                        #"
+	echo "#    https://github.com/zar-nicolas20/   #"
+	echo "#    GlyphsApp-Package-Install/          #"
+	echo "##########################################"
+fi
